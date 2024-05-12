862606f25195604ae171d092723257dfb8a5e359
ameya28.dev <ameya28.mahale@gmail.com>
2024-05-12 16:36:23 +0530

    valgrind

---
+++ valgrind/v.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <string>
+class Node
+{
+public:
+	std::string name;
+	int age;
+
+	Node()
+	{
+		this->name = "mason";
+		this->age = 0;
+	}
+
+	Node(std::string name, int age)
+	{
+		this->name = name;
+		this->age = age;
+	}
+};
+
+Node createNode(std::string name, int age)
+{
+	return Node(name, age);
+}
+
+Node* createHeapNode(std::string name, int age)
+{
+	Node* node = new Node(name, age);
+	return node;
+}
+
+int main()
+{
+
+	std::vector<int> v{};
+
+	int *ptr = new int[5];
+	delete[] ptr;
+
+	createNode("mason", 15);
+	Node* n = createHeapNode("mason", 15);
+	delete n;
+	return 0;
+}
---
+++ valgrind/val.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+int main(){
+	char* s = (char*) malloc(5*sizeof(char));
+	free(s);
+	return 0;
+}
