272f452180f057f25c21494fde5352f55ab1b0b5
Ameya G Mahale <ameya28.mahale@outlook.com>
2025-06-12 23:13:22 +0530

    singleton

--- singleton.cpp
+++ singleton.cpp
@@ -1,39 +1,24 @@
-#include <iostream>
-// class Singleton {
-//    public:
-//     static Singleton& get() {
-//         return s_instance;
-//     }
-
-//    private:
-//     Singleton() {}
-
-//     static Singleton s_instance;
-// };
-
-class StaticClass {
+class Random {
    public:
-    StaticClass() { value = 0; }
-    StaticClass(int val) { value = val; }
-    int get() const { return value; }
-    void update(int val) const { value = val; }
-    void print() const { std::cout << value << std::endl; }
+    Random(const Random& other) = delete;
+
+    static Random& Get() {
+        static Random instance;
+        return instance;
+    }
+
+    static float Float() { return Get().FloatImpl(); }
 
    private:
-    static int value;
+    Random() {};
+
+    float FloatImpl() { return random_number; }
+
+    float random_number = 0.5f;
 };
 
-int StaticClass::value;
-
 int main() {
-    StaticClass s1;
-
-    s1.update(2);
-
-    StaticClass s2(3);
-
-    s1.print();
-    s2.print();
-
-    return 0;
+    Random& instance = Random::Get();
+    auto number = Random::Float();
+    auto num = instance.Float();
 }
